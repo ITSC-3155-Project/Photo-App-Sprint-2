3c91b4586b8d2d9c7d91592780feb2c0feb9245f
Alexander Wilson <awils235@charlotte.edu>
2025-12-07 23:09:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+// src/App.jsx
 import React, { useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-
-import "../styles/main.css";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { Grid } from "@mui/material";
 
 import TopBar from "./components/topBar/TopBar.jsx";
 import UserList from "./components/userList/userList.jsx";
@@ -12,78 +12,65 @@ import LoginRegister from "./components/loginRegister/loginRegister.jsx";
 function App() {
   const [loggedInUser, setLoggedInUser] = useState(null);
 
+  const isLoggedIn = !!loggedInUser;
+
+  const handleLogin = (user) => {
+    // called by LoginRegister when /admin/login succeeds
+    setLoggedInUser(user);
+  };
+
+  const handleLogout = () => {
+    // TopBar will also POST /admin/logout; this just clears local state
+    setLoggedInUser(null);
+  };
+
   return (
-    <div className="cs142-main">
-      <TopBar
-        loggedInUser={loggedInUser}
-        setLoggedInUser={setLoggedInUser}
-      />
+    <div>
+      <TopBar loggedInUser={loggedInUser} onLogout={handleLogout} />
 
-      <div className="cs142-content">
-        <Routes>
-          {/* Login/register is always reachable */}
-          <Route
-            path="/login-register"
-            element={<LoginRegister setLoggedInUser={setLoggedInUser} />}
-          />
+      <Grid container>
+        {/* Left column: user list – only when logged in */}
+        <Grid item xs={3}>
+          {isLoggedIn && <UserList />}
+        </Grid>
 
-          {/* Everything below this requires login */}
-          {loggedInUser ? (
-            <>
-              <Route
-                path="/users/:userId"
-                element={
-                  <div className="cs142-main-content">
-                    <div className="cs142-left-pane">
-                      <UserList />
-                    </div>
-                    <div className="cs142-right-pane">
-                      <UserDetail />
-                    </div>
-                  </div>
-                }
-              />
+        {/* Right column: main view */}
+        <Grid item xs={9}>
+          <Switch>
+            {/* Login / register page */}
+            <Route path="/login-register">
+              {isLoggedIn ? (
+                // already logged in – go to their details page
+                <Redirect to={`/users/${loggedInUser._id}`} />
+              ) : (
+                <LoginRegister
+                  onLogin={handleLogin}
+                  setLoggedInUser={setLoggedInUser}
+                />
+              )}
+            </Route>
 
-              <Route
-                path="/photos/:userId"
-                element={
-                  <div className="cs142-main-content">
-                    <div className="cs142-left-pane">
-                      <UserList />
-                    </div>
-                    <div className="cs142-right-pane">
-                      <UserPhotos />
-                    </div>
-                  </div>
-                }
-              />
+            {/* User detail – protected */}
+            <Route path="/users/:userId">
+              {isLoggedIn ? <UserDetail /> : <Redirect to="/login-register" />}
+            </Route>
 
-              <Route
-                path="/users"
-                element={
-                  <div className="cs142-main-content">
-                    <div className="cs142-left-pane">
-                      <UserList />
-                    </div>
-                    <div className="cs142-right-pane">
-                      <div>Please select a user.</div>
-                    </div>
-                  </div>
-                }
-              />
+            {/* User photos – protected */}
+            <Route path="/photos/:userId">
+              {isLoggedIn ? <UserPhotos /> : <Redirect to="/login-register" />}
+            </Route>
 
-              {/* Default when logged in: go to own user page */}
-              <Route
-                path="*"
-                element={<Navigate to={`/users/${loggedInUser._id}`} replace />}
-              />
-            </>
-          ) : (
-            // If not logged in, force any route to login-register
-            <Route path="*" element={<Navigate to="/login-register" replace />} />
-          )}
-        </Routes>
-      </div>
+            {/* Default route */}
+            <Route path="/">
+              {isLoggedIn ? (
+                <Redirect to={`/users/${loggedInUser._id}`} />
+              ) : (
+                <Redirect to="/login-register" />
+              )}
+            </Route>
+          </Switch>
+        </Grid>
+      </Grid>
     </div>
   );
 }
